b2f9af8b31fc0f6df4b6210545cc91f6fc36cb55
AdrianD5612 <adriand5612@gmail.com>
2024-02-22 19:14:45 +0100

    Better navigation & spacing

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -24,13 +24,13 @@ export default function Home() {
 
     if (!finished) return  <div className="flex justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">Ładowanie, proszę czekać...</div>
     return (
-        <main className="flex flex-col items-center justify-center p-24 border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4 lg:bg-zinc-800/30">
+        <main className="flex flex-col gap-1 items-center justify-center p-24 border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4 lg:bg-zinc-800/30">
     
-          <div className="z-10 w-0 from-black via-black items-center justify-center font-mono text-sm lg:flex">
-            
+          <div className=" from-black via-black items-center justify-center font-mono text-sm lg:flex">
+            <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300 py-2 px-4 rounded-full" onClick={() => router.push("/")}>Strona główna</button>
           </div>
 
-          <div className="-z-5 max-w-5xl w-full from-black via-black items-center justify-center font-mono text-sm lg:flex">
+          <div className="max-w-5xl w-full from-black via-black items-center justify-center font-mono text-sm lg:flex">
             <table className="text-white">
               <thead>
                 <tr>

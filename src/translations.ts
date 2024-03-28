86f6fe7c60fb61023ec5fa97e11e6cdf8518229c
AdrianD5612 <adriand5612@gmail.com>
2024-03-28 16:58:56 +0100

    Translations file comment improvements

--- src/translations.ts
+++ src/translations.ts
@@ -8,20 +8,21 @@ const translations = {
 
 let lang: "en" | "pl";
 
+
 /**
- * Get a translation for a given key in a specific locale.
+ * Retrieves the translation for a given key.
  *
- * @param {string} locale - The locale to use for translation.
- * @param {string} key - The translation key to lookup.
- * @throws {Error} Throws an error if the locale or key is unknown.
- * @return {string} The translated value.
+ * @param {string} key - The key for the translation to retrieve.
+ * @return {string} The translation corresponding to the key.
  */
 export function getTranslation(key: string): string {
   if (lang == undefined) {
+    // check if we're in the browser
     if (typeof window !== 'undefined' && navigator) {
-      // set polish or in any other case english
+      // check and set polish or in any other case english
       lang = navigator.language.slice(0, 2)==="pl" ? "pl" : "en";
     } else {
+      // in server-side case set lang to english
       lang = "en";
     }
   }
@@ -35,6 +36,10 @@ export function getTranslation(key: string): string {
   return localeData[key];
 }
 
+/**
+ * Toggles the language between English and Polish.
+ *
+ */
 export function switchLang() {
   if (lang === "en") {
     lang = "pl";
